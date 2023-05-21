dd67bd4aa9aa32a63fff9162e501017051b005c4
Jakub Pawlak <j.pawlak@student.fontys.nl>
2023-05-21 22:41:00 +0200

    Changed the script to generate visitor

--- Automata/compile_grammars.sh
+++ Automata/compile_grammars.sh
@@ -7,7 +7,7 @@ for file in $(find "$script_dir/Grammar" -type f -name "*.g4"); do
   file_name=${file##*/Grammar/}
   read -r namespace < <(echo "Grammar/${file_name%/*.g4}" | tr "/" ".")
   echo "Compiling $file_name"
-  antlr -package "$namespace" -Dlanguage=CSharp "$file"
+  antlr -visitor -package "$namespace" -Dlanguage=CSharp "$file"
 done
 
 find "$script_dir/Grammar" -type f -name "*.cs" -exec git add {} \;
