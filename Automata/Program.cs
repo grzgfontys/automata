d46a52e0b36b3ea98a833682decf801a1738bde4
Jakub Pawlak <j.pawlak@student.fontys.nl>
2023-05-21 23:17:58 +0200

    Added the main function

--- Automata/Program.cs
+++ Automata/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using Automata.Parsing;
+using Grammar.Assignment1;
 
-Console.WriteLine("Hello, World!");
+namespace Automata;
+
+public static class Program
+{
+	private const string FilePath = "test.txt";
+
+	public static void Main(string[] args)
+	{
+		using FileStream file = File.OpenRead(FilePath);
+		ICharStream stream = CharStreams.fromStream(file);
+		ITokenSource lexer = new Assignment1Lexer(stream);
+		ITokenStream tokens = new CommonTokenStream(lexer);
+		var parser = new Assignment1Parser(tokens);
+
+		IParseTree tree = parser.file();
+		FileVisotor visitor = new();
+
+		var result = visitor.Visit(tree) ?? Enumerable.Empty<Human>();
+		foreach ( Human human in result )
+		{
+			Console.WriteLine(human);
+		}
+	}
+}
