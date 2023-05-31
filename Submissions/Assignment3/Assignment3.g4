52413b29f7241abd69b280a79b271bf5b91f22a9
Grzegorz Malisz <g.malisz@student.fontys.nl>
2023-05-31 15:30:37 +0200

    Finished for submission

---
+++ Submissions/Assignment3/Assignment3.g4
@@ -0,0 +1,41 @@
+grammar Assignment3;
+
+file
+    : line*EOF;
+    
+line
+    : keywords '(' (expression ','?)+ ')'                   # InvokeFunction
+    | varAssignment                                         # VariableAssignment
+    | expression                                            # LoneExpression
+    ;
+
+varAssignment
+    : VARIABLE op='=' NUMBER                # varAssignmentNumber
+    | VARIABLE op='=' expression            # varAssignmentExpression
+    ;
+
+expression 
+    : NUMBER                                 # Literal
+    | VARIABLE                               # NestedVar
+    | expression '!'                         # Factorial
+    | expression op='^' expression           # BinaryOperation
+    | expression op=('*'|'/') expression     # BinaryOperation
+    | expression op=('+'|'-') expression     # BinaryOperation
+    | '(' expression ')'                     # ParenthesizedExpression
+    ;
+    
+keywords
+    : 'print'       # Print
+    ;
+
+// Może być to albo tak jak zrobiłem w keywords, wychodzi na to że on z pierwszeństwem traktuje wyszukiwanie konkretnych stringów jako tokenów w parsing rules a nie w lexer rules
+//PRINT       : 'print';
+NUMBER      : NONZERO_DIGIT DIGIT* | ZERO;
+VARIABLE    : LETTERS+;
+WHITESPACE  : [ \t\n\r]+ -> skip;
+NEWLINE     : '\r'? '\n';
+
+fragment NONZERO_DIGIT  : [1-9];
+fragment ZERO           : '0';
+fragment DIGIT          : ZERO | NONZERO_DIGIT;
+fragment LETTERS        : [a-zA-Z];
---
+++ Submissions/Assignment3/Listener.cs
@@ -0,0 +1,88 @@
+﻿using Antlr4.Runtime.Tree;
+using static System.Math;
+
+namespace Automata.Parsing.Assignment3;
+
+public class Listener : Assignment3BaseListener
+{
+    public Dictionary<IRuleNode, int> values = new();
+    public Dictionary<string, int> variables = new();
+    
+    public int GetResult(Assignment3Parser.ExpressionContext expr) => values[expr];
+
+    public void PrintVariables()
+    {
+        foreach (KeyValuePair<string,int> variable in variables)
+        {
+            Console.WriteLine($"{variable.Key} = {variable.Value}");
+        }
+    }
+    
+    public override void ExitInvokeFunction(Assignment3Parser.InvokeFunctionContext context)
+    {
+        switch (context.keywords().GetText())
+        {
+            case "print":
+                Console.WriteLine(values[context.expression(0)]);
+                break;
+        }
+    }
+    
+    public override void ExitVarAssignmentNumber(Assignment3Parser.VarAssignmentNumberContext context)
+    {
+        string varName = context.VARIABLE().GetText();
+        int value = int.Parse(context.NUMBER().GetText());
+        variables[varName] = value;
+    }
+    
+    public override void ExitVarAssignmentExpression(Assignment3Parser.VarAssignmentExpressionContext context)
+    {
+        string varName = context.VARIABLE().GetText();
+        int value = values[context.expression()];
+        variables[varName] = value;
+    }
+    
+    public override void ExitNestedVar(Assignment3Parser.NestedVarContext context)
+    {
+        int value = variables[context.VARIABLE().GetText()];
+        values[context] = value;
+    }
+    
+    public override void ExitLiteral(Assignment3Parser.LiteralContext context)
+    {
+        int value = int.Parse(context.NUMBER().GetText());
+        values[context] = value;
+    }
+
+    public override void ExitParenthesizedExpression(Assignment3Parser.ParenthesizedExpressionContext context)
+    {
+        values[context] = values[context.expression()];
+    }
+
+    public override void ExitFactorial(Assignment3Parser.FactorialContext context)
+    {
+        int value = values[context.expression()];
+        int result = Factorial(value);
+        values[context] = result;
+    }
+
+    public override void ExitBinaryOperation(Assignment3Parser.BinaryOperationContext context)
+    {
+        const string powerOperator = "^";
+		
+        int lhs = values[context.expression(0)];
+        int rhs = values[context.expression(1)];
+        string op = context.op.Text;
+        values[context] = op switch
+        {
+            "+"           => lhs + rhs,
+            "-"           => lhs - rhs,
+            "*"           => lhs * rhs,
+            "/"           => lhs / rhs,
+            powerOperator => (int) Pow(lhs, rhs),
+            _             => throw new ArgumentException($"Unknown binary operator {op}")
+        };
+    }
+
+    private static int Factorial(int n) => Enumerable.Range(1, n).Aggregate(1, (acc, i) => acc * i);
+}
