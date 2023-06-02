515cb3504d5e6242003901f95a9ec5304619c9c9
Jakub Pawlak <j.pawlak@student.fontys.nl>
2023-06-02 16:11:26 +0200

    Added assignment 4 to main

--- Automata/Program.cs
+++ Automata/Program.cs
@@ -1,9 +1,11 @@
 ﻿using Antlr4.Runtime;
 using Antlr4.Runtime.Tree;
 using Automata.Parsing.Assignment3;
+using Automata.Parsing.Calculator;
 using Automata.Parsing.Math;
 using Grammar.Assignment2;
 using Grammar.Assignment3;
+using Grammar.Assignment4;
 
 namespace Automata;
 
@@ -19,26 +21,29 @@ public static class Program
 	{
 		bool useVisitor = false;
 
-		ICharStream stream = CharStreams.fromString(expression);
-		ITokenSource lexer = new MathLexer(stream);
-		ITokenStream tokens = new CommonTokenStream(lexer);
-		var parser = new MathParser(tokens);
-		IParseTree tree = parser.expression();
-
-		int result;
-		if ( useVisitor )
+		foreach ( string expression in Expressions )
 		{
-			MathVisitor visitor = new();
-			result = visitor.Visit(tree);
-		}
-		else
-		{
-			MathListener listener = new();
-			ParseTreeWalker.Default.Walk(listener, tree);
-			result = listener.GetResult((MathParser.ExpressionContext) tree);
-		}
+			ICharStream stream = CharStreams.fromString(expression);
+			ITokenSource lexer = new MathLexer(stream);
+			ITokenStream tokens = new CommonTokenStream(lexer);
+			var parser = new MathParser(tokens);
+			IParseTree tree = parser.expression();
 
-		Console.WriteLine($"{expression} = {result}");
+			int result;
+			if ( useVisitor )
+			{
+				MathVisitor visitor = new();
+				result = visitor.Visit(tree);
+			}
+			else
+			{
+				MathListener listener = new();
+				ParseTreeWalker.Default.Walk(listener, tree);
+				result = listener.GetResult((MathParser.ExpressionContext) tree);
+			}
+
+			Console.WriteLine($"{expression} = {result}");
+		}
 	}
 
 	private static void Assignment3(string input)
@@ -53,15 +58,28 @@ public static class Program
 		Listener listener = new();
 		walker.Walk(listener, tree);
 	}
+	
+	private static void Assignment4(string input)
+	{
+		AntlrInputStream stream = new( input );
+		ITokenSource lexer = new CalculatorLexer( stream );
+		CommonTokenStream tokens = new( lexer );
+		CalculatorParser parser = new( tokens );
+		IParseTree tree = parser.file();
+		
+		var calculator = new  Calculator();
+		calculator.Visit(tree);
+	}
 
 	public static void Main(string[] args)
 	{
+		string input = File.ReadAllText(@"input/assignment4.txt");
 		Console.WriteLine("---------input---------");
-		Console.WriteLine(Input);
+		Console.WriteLine(input);
 		Console.WriteLine("-----------------------");
 
 		Console.WriteLine("##########_ANTLR output_##########");
-		Assignment3(Input);
+		Assignment4(input);
 		Console.WriteLine("##################################");
 	}
 }
