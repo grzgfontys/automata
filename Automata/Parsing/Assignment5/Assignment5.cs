abeadcd45aac783fcc74aa48cf490dc5782b1c5b
Jakub Pawlak <j.pawlak@student.fontys.nl>
2023-06-08 15:47:43 +0100

    Code cleanup and refactoring

--- Automata/Parsing/Assignment5/Assignment5.cs
+++ Automata/Parsing/Assignment5/Assignment5.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using Antlr4.Runtime.Tree;
+
 // using Grammar.Assignment5;
 
 namespace Automata.Parsing.Assignment5;
@@ -7,28 +8,24 @@ namespace Automata.Parsing.Assignment5;
 public class Assignment5CustomVisitor : Assignment5BaseVisitor<object?> // nullable object because we do not return any value
 {
 	private readonly IDictionary<string, int> _variableValues = new Dictionary<string, int>();
-	private readonly HashSet<string> _variableInit = new HashSet<string>();
+	private readonly HashSet<string> _variableInit = new();
+
 	private IDictionary<string, int> _functionsVariables = new Dictionary<string, int>();
+
 	// Stores function context for executing code inside the function by name of function
 	private readonly IDictionary<string, IRuleNode> _functionsContexts = new Dictionary<string, IRuleNode>();
+
 	// Stores names of function, then in list are parameters for function, one parameter in tuple name and value
-	private readonly IDictionary<string, List<Tuple<string,int?>>> _declaredFunctions = new Dictionary<string, List<Tuple<string,int?>>>();
+	private readonly IDictionary<string, List<Tuple<string, int?>>> _declaredFunctions =
+		new Dictionary<string, List<Tuple<string, int?>>>();
+
 	private readonly IAssignment5Visitor<int> _intVisitor;
 	private readonly IAssignment5Visitor<bool> _boolVisitor;
 
-	class FunParam
+	private class FunParam
 	{
-		public readonly string Name;
-		public readonly int? Value;
-		public FunParam(string name, int value)
-		{
-			this.Name = name;
-			this.Value = value;
-		}
-		public FunParam(string name)
-		{
-			this.Name = name;
-		}
+		public required string Name { get; init; }
+		public int? Value { get; init; }
 	}
 
 	public Assignment5CustomVisitor()
@@ -43,10 +40,10 @@ public class Assignment5CustomVisitor : Assignment5BaseVisitor<object?> // nulla
 		_intVisitor = new IntegralExpressionVisitor(_variableValues);
 		_boolVisitor = new BooleanExpressionVisitor(_intVisitor);
 	}
-	
-	private object? HelperVisitor(IRuleNode scopedStatementBlockContext, IDictionary<string, int> variableValues)
+
+	private static object? HelperVisitor(IParseTree scopedStatementBlockContext, IDictionary<string, int> variableValues)
 	{
-		var visitor = new  Assignment5CustomVisitor(variableValues);
+		var visitor = new Assignment5CustomVisitor(variableValues);
 		return visitor.Visit(scopedStatementBlockContext);
 	}
 
@@ -84,59 +81,61 @@ public class Assignment5CustomVisitor : Assignment5BaseVisitor<object?> // nulla
 			Console.WriteLine(_intVisitor.Visit(expression));
 		}
 	}
-	private Dictionary<string, int> HandleUserFunction(string funName, IEnumerable<Assignment5Parser.ExpressionContext> expressions)
+
+	private Dictionary<string, int> HandleUserFunction(string funName,
+	                                                   IEnumerable<Assignment5Parser.ExpressionContext> expressions)
 	{
-		List<Assignment5Parser.ExpressionContext> expressionsList = expressions.ToList();
-		Dictionary<string, int> functionsVariables = new ();
-		if(!_declaredFunctions.ContainsKey(funName))
+		var expressionsList = expressions.ToList();
+		Dictionary<string, int> functionsVariables = new();
+		if ( !_declaredFunctions.ContainsKey(funName) )
 		{
+			// TODO: This is not unreachable exception
 			throw new UnreachableException($"Function {funName} does not exist");
-		} 
-		else if(_declaredFunctions[funName].Count < expressionsList.Count)
+		}
+		if ( _declaredFunctions[funName].Count < expressionsList.Count )
 		{
-			
+			// TODO: This is not unreachable exception
 			throw new UnreachableException($"Function {funName} takes less parameters");
 		}
-		else
+		int j = 0;
+		for ( int i = 0; i < expressionsList.Count; i++ )
 		{
-			int j = 0;
-			for (int i = 0; i < expressionsList.Count; i++)
-			{
-				functionsVariables[_declaredFunctions[funName][i].Item1] = _intVisitor.Visit(expressionsList[i]);
-				j = i;
-			}
-
-			j++;
-
-			if (j < _declaredFunctions[funName].Count)
-			{
-				for (int i = j; i < _declaredFunctions[funName].Count; i++)
-				{
-					int? defaultParam = _declaredFunctions[funName][i].Item2;
-					if (defaultParam.HasValue)
-					{
-						functionsVariables[_declaredFunctions[funName][i].Item1] = defaultParam.Value;
-					}
-					else
-					{
-						throw new UnreachableException($"Function {funName} takes more parameters");
-					}
-				}
-			}
-
-			return functionsVariables;
+			functionsVariables[_declaredFunctions[funName][i].Item1] = _intVisitor.Visit(expressionsList[i]);
+			j = i;
 		}
+
+		j++;
+
+		if ( j >= _declaredFunctions[funName].Count )
+			return functionsVariables;
+
+		for ( int i = j; i < _declaredFunctions[funName].Count; i++ )
+		{
+			int? defaultParam = _declaredFunctions[funName][i].Item2;
+			if ( defaultParam.HasValue )
+			{
+				functionsVariables[_declaredFunctions[funName][i].Item1] = defaultParam.Value;
+			}
+			else
+			{
+				// TODO: This is not unreachable exception
+				throw new UnreachableException($"Function {funName} takes more parameters");
+			}
+		}
+
+		return functionsVariables;
 	}
 
 	public override object? VisitInitialisation(Assignment5Parser.InitialisationContext context)
 	{
 		string varName = context.IDENT().GetText();
-		if (!_variableInit.Contains(varName) && !_variableValues.ContainsKey(varName))
+		if ( !_variableInit.Contains(varName) && !_variableValues.ContainsKey(varName) )
 		{
 			_variableInit.Add(varName);
 		}
 		else
 		{
+			// TODO: This is not unreachable exception
 			throw new UnreachableException($"Second initialization of variable: {varName}");
 		}
 		return null;
@@ -150,13 +149,13 @@ public class Assignment5CustomVisitor : Assignment5BaseVisitor<object?> // nulla
 		_variableValues[varName] = value;
 		return null;
 	}
-	
+
 	public override object? VisitFunctionAssignment(Assignment5Parser.FunctionAssignmentContext context)
 	{
 		string varName = context.IDENT().GetText();
-		var value = this.Visit(context.functionCall());
+		var value = Visit(context.functionCall());
 
-		if (value is int intValue)
+		if ( value is int intValue )
 		{
 			_variableValues[varName] = intValue;
 		}
@@ -165,35 +164,37 @@ public class Assignment5CustomVisitor : Assignment5BaseVisitor<object?> // nulla
 
 	public override object VisitDefaultParam(Assignment5Parser.DefaultParamContext context)
 	{
-		string paramName = context.IDENT().GetText();
-		int paramValue = _intVisitor.Visit(context.expression());
-		
-		return new FunParam(paramName, paramValue);
+		return new FunParam
+		{
+			Name = context.IDENT().GetText(), 
+			Value = _intVisitor.Visit(context.expression())
+		};
 	}
 
 	public override object VisitParam(Assignment5Parser.ParamContext context)
 	{
-		string paramName = context.IDENT().GetText();
-		
-		return new FunParam(paramName);
+		return new FunParam
+		{
+			Name = context.IDENT().GetText()
+		};
 	}
 
 	public override object? VisitFunctionDeclaration(Assignment5Parser.FunctionDeclarationContext context)
 	{
 		string functionName = context.functionName().GetText();
-		_declaredFunctions[functionName] = new List<Tuple<string,int?>>();
+		_declaredFunctions[functionName] = new List<Tuple<string, int?>>();
 		_functionsContexts[functionName] = context.statementBlock();
 
-		var paramsCount = context.functionParams().Length; 
+		var paramsCount = context.functionParams().Length;
 
-		for (int i = 0; i < paramsCount; i++)
+		for ( int i = 0; i < paramsCount; i++ )
 		{
-			FunParam? funParam = (FunParam?)Visit(context.functionParams(i));
-			if (funParam != null)
+			FunParam? funParam = (FunParam?) Visit(context.functionParams(i));
+			if ( funParam is not null )
 				_declaredFunctions[functionName].Add(new Tuple<string, int?>(funParam.Name, funParam.Value));
 		}
 
-		Console.WriteLine(String.Join(",", _declaredFunctions[functionName]));
+		Console.WriteLine(string.Join(",", _declaredFunctions[functionName]));
 		return null;
 	}
 
@@ -208,7 +209,7 @@ public class Assignment5CustomVisitor : Assignment5BaseVisitor<object?> // nulla
 			case Assignment5Parser.IDENT:
 				_functionsVariables = HandleUserFunction(context.functionName().IDENT().GetText(), context.expression());
 				// this.Visit(_functionsContexts[context.functionName().IDENT().GetText()]);
-				var value = this.HelperVisitor(_functionsContexts[context.functionName().IDENT().GetText()], _functionsVariables);
+				var value = HelperVisitor(_functionsContexts[context.functionName().IDENT().GetText()], _functionsVariables);
 				_functionsVariables.Clear();
 				return value;
 			default:
@@ -216,17 +217,17 @@ public class Assignment5CustomVisitor : Assignment5BaseVisitor<object?> // nulla
 		}
 		return null;
 	}
-	
+
 	public override object? VisitStatementBlock(Assignment5Parser.StatementBlockContext context)
 	{
 		int childCount = context.ChildCount;
-		for (int i = 0; i < childCount; ++i)
+		for ( int i = 0; i < childCount; ++i )
 		{
-			if(context.GetChild(i).GetType() == typeof(Assignment5Parser.ReturnStatementContext))
+			if ( context.GetChild(i) is Assignment5Parser.ReturnStatementContext )
 			{
-				return this.Visit(context.GetChild(i));
+				return Visit(context.GetChild(i));
 			}
-			this.Visit(context.GetChild(i));
+			Visit(context.GetChild(i));
 		}
 		return null;
 	}
@@ -234,7 +235,7 @@ public class Assignment5CustomVisitor : Assignment5BaseVisitor<object?> // nulla
 	public override object? VisitReturnStatement(Assignment5Parser.ReturnStatementContext context)
 	{
 		int? value = null;
-		if (context.expression() != null)
+		if ( context.expression() is not null )
 		{
 			value = _intVisitor.Visit(context.expression());
 		}
